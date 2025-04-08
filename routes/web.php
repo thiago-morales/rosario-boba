5c24f48d8cb974313547a97b95c4f7c9c7ecd880
John Doe <johndoe@example.com>
2025-04-08 20:35:48 -0300

    web vieja

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+route::get('/hombre', function () {
+    return "ROSARIO ES NOVIA DE MARCOS Y BAUTISTA A LA VEZ";
+});
